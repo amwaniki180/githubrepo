88d91716637c8f17cbb316382524145c5404dc7e
Antony <amwaniki180@gmail.com>
2019-06-07 15:50:21 +0300

    private services

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import{Http,Header} from 'angular/Http';
+import'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private username:string;
+  private clientid ='Iv1.45e1e8e0227ce824';
+  private clientsecret = '42ba8662bd4a491a0dee7ed0388c264d4e4c50a9';
 
-  constructor() { }
+
+  constructor(private _http:Http) {
+    console.log("service is ready!");
+    this.username = 'amwaniki';
+   }
+   getProfileInfo(){
+     return this._http.get("")
+   }
 }
